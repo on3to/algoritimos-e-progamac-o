0c35e4ad2030074241a7d70394f177a515acb3a4
F-Neto15 <chicaoneto15@gmail.com>
2026-02-10 20:14:20 -0300

    ate a 14

--- 14-calcular a nota ponderada.py
+++ 14-calcular a nota ponderada.py
@@ -6,5 +6,20 @@ n2=int(input("Qual a segunda nota do aluno?"))
 
 n3=int(input("Qual a terceira nota do aluno?"))
 
-#Processamento 
+p1=2
 
+p2=3
+
+p3=4
+
+#Processamento
+
+nota=(n1 * p1) + (n2 * p2) + (n3 * p3)
+
+peso= p1 + p2 +p3
+
+notafinal= nota / peso
+
+#saida
+
+print(f"A media ponderada sera de:{notafinal}")
