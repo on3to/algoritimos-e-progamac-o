004a011747e2645578260dbc9be531e7cdfb7e3f
F-Neto15 <chicaoneto15@gmail.com>
2026-02-25 23:33:56 -0300

    tudo

---
+++ algoritimo mudulo 4 condicionais/caluladora.py
@@ -0,0 +1,18 @@
+def main():
+
+
+n1 = float(input("qual o primeiro numero?"))
+n2 = float(input("qual o segundo numero?"))                  
+
+print("escolha a operação")
+print("1 soma")
+print("2 subtração")
+print("3 multiplicação")
+print("4 divisão")
+
+input("qual o numero da operação de 1-4?")
+
+
+
+
+main()
