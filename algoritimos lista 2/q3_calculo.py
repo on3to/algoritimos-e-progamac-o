941b014c8ffb82260dc3b7d007ae050b01ece03b
F-Neto15 <chicaoneto15@gmail.com>
2026-03-02 19:34:47 -0300

    ate a questão 4

--- algoritimos lista 2/q3_calculo.py
+++ algoritimos lista 2/q3_calculo.py
@@ -1,9 +1,11 @@
-def verificar(a, b):
+def verificar(a, b, c):
 
-    if a > b:
+    if a > b and a > c:
         return ("o primeiro numero e maior")
-    elif b > a:
+    elif b > a and b> c:
         return ("o segundo numero e maior")
+    elif c > a and c > b:
+        return ("o terceiro numero e maioir")
     else:
-        return ("os numeros são iguais")
+        return("os numeros são iguais")
     
--- algoritimos lista 2/questão 3.py
+++ algoritimos lista 2/questão 3.py
@@ -4,8 +4,9 @@ def main():
 
     n1 = float(input("qual o primeiro numero:"))
     n2 = float(input("qual o segundo numero:"))
+    n3 = float(input("qual o terceiro numero:"))
 
-    resultado = verificar(n1, n2)
+    resultado = verificar(n1, n2, n3)
     print(resultado)
 
 main()
